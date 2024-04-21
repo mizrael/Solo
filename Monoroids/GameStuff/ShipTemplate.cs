0316aef3e75708f901f09c98dea28cb417e807bb
David Guida <1432872+mizrael@users.noreply.github.com>
2024-04-21 19:24:47 -0400

    refactoring

--- Monoroids/GameStuff/ShipTemplate.cs
+++ Monoroids/GameStuff/ShipTemplate.cs
@@ -1,15 +1,3 @@
-using System;
-
 namespace Monoroids.GameStuff;
 
 public record struct ShipTemplate(string Name, string Asset, PlayerStats Stats);
-
-public class GameState
-{
-    private GameState(){}
-
-    public ShipTemplate ShipTemplate;
-
-    private static Lazy<GameState> _instance = new(new GameState());
-    public static GameState Instance => _instance.Value;
-}
