dcb864102673dadf112543cee2a5d263ff318a49
David Guida <1432872+mizrael@users.noreply.github.com>
2024-04-20 23:15:25 -0400

    added ship selection UI

---
+++ Monoroids/GameStuff/Components/ShipSelectionUIComponent.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Monoroids.Core;
+using Monoroids.Core.Components;
+using Monoroids.Core.Services;
+
+namespace Monoroids.GameStuff.Components;
+
+public class ShipSelectionUIComponent : Component, IRenderable
+{
+    private RenderService? _renderService;
+
+    private static string[] _text = [
+        "Select your ship",
+        "Press ENTER to start"
+    ];
+
+    private ShipSelectionUIComponent(GameObject owner) : base(owner)
+    {
+    }
+
+    protected override void InitCore()
+    {
+        _renderService = GameServicesManager.Instance.GetService<RenderService>();
+    }
+
+    public void Render(SpriteBatch spriteBatch)
+    {
+        var scale = 2f;
+
+        var prevLineHeight = 0f;
+        foreach(var line in _text)
+        {
+            var lineSize = Font!.MeasureString(line) * scale * .5f;
+
+            var pos = new Vector2(_renderService!.Graphics.PreferredBackBufferWidth * .5f - lineSize.X,
+                                  (lineSize.Y + prevLineHeight) * scale);
+
+            spriteBatch.DrawString(Font, line, pos, Color.White,
+                                   0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+
+            prevLineHeight = lineSize.Y;
+        }   
+    }
+
+    public SpriteFont? Font;
+
+    public int LayerIndex { get; set; }
+    public bool Hidden { get; set; }
+}
--- Monoroids/GameStuff/Scenes/ShipSelectionScene.cs
+++ Monoroids/GameStuff/Scenes/ShipSelectionScene.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.Xna.Framework;
-using Monoroids.Core.Components;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using Monoroids.Core;
-using Monoroids.Core.Services;
-using System;
 using Monoroids.Core.Assets;
 using Monoroids.Core.Assets.Loaders;
-using System.Collections.Generic;
+using Monoroids.Core.Components;
+using Monoroids.Core.Services;
+using Monoroids.GameStuff.Components;
+using System;
 using System.Linq;
 
 namespace Monoroids.GameStuff.Scenes;
@@ -17,8 +19,8 @@ public class ShipSelectionScene : Scene
     private static readonly ShipMeta[] _shipsMeta =
     [
         new ShipMeta("playerShip2_red"),
-        //new Ship("playerShip2_green"),
-        //new Ship("playerShip2_blue"),
+        new ShipMeta("playerShip2_green"),
+        new ShipMeta("playerShip2_blue"),
     ];
 
     private GameObject[] _ships;
@@ -34,34 +36,74 @@ public class ShipSelectionScene : Scene
         this.Root.AddChild(background);
 
         var spriteSheet = new SpriteSheetLoader().Load("meta/sheet.json", Game);
-
-        var shipsContainer = new GameObject();
-        this.Root.AddChild(shipsContainer);
-
-        _ships = BuildShips(spriteSheet);
-        foreach (var ship in _ships)
-            shipsContainer.AddChild(ship);
+                     
+        BuildShips(spriteSheet);
+        
+        BuildUI();
 
         SelectShip(0);
     }
 
+    private void BuildUI()
+    {
+        var ui = new GameObject();
+
+        var textComponent = ui.Components.Add<ShipSelectionUIComponent>();
+        textComponent.LayerIndex = (int)RenderLayers.UI;        
+        textComponent.Font = Game.Content.Load<SpriteFont>("Fonts/UI");
+
+        KeyboardState prevKeyState = new();
+        var brain = ui.Components.Add<LambdaComponent>();
+        brain.OnUpdate = (owner, gameTime) =>
+        {
+            var keyboardState = Keyboard.GetState();
+
+            if(prevKeyState.IsKeyDown(Keys.Left) && keyboardState.IsKeyUp(Keys.Left))
+            {
+                SelectShip(_selectedShipIndex - 1);
+            }
+            else if (prevKeyState.IsKeyDown(Keys.Right) && keyboardState.IsKeyUp(Keys.Right))
+            {
+                SelectShip(_selectedShipIndex + 1);
+            }
+            else if (prevKeyState.IsKeyDown(Keys.Enter) && keyboardState.IsKeyUp(Keys.Enter))
+            {
+                GameServicesManager.Instance.GetService<SceneManager>().SetCurrentScene(SceneNames.Play);
+            }
+
+            prevKeyState = keyboardState;
+        };
+
+        this.Root.AddChild(ui);
+    }
+
     private void SelectShip(int index)
     {
+        index = (index < 0) ? _ships.Length - 1 : index % _ships.Length;
+
         _ships[_selectedShipIndex].Enabled = false;
         _selectedShipIndex = index;
         _ships[_selectedShipIndex].Enabled = true;
     }
 
-    private GameObject[] BuildShips(SpriteSheet spriteSheet)
+    private void BuildShips(SpriteSheet spriteSheet)
     {
+        var shipsContainer = new GameObject();
+
         var renderService = GameServicesManager.Instance.GetService<RenderService>();
 
         var halfSize = new Vector2(renderService.Graphics.PreferredBackBufferWidth / 2,
-                                       renderService.Graphics.PreferredBackBufferHeight / 2);
+                                    renderService.Graphics.PreferredBackBufferHeight / 2);
         var radius = 25f;
         var speed = 0.005f;
 
-        return _shipsMeta.Select(s =>
+        renderService.Graphics.DeviceReset += (s, e) =>
+        {
+            halfSize = new Vector2(renderService.Graphics.PreferredBackBufferWidth / 2,
+                                    renderService.Graphics.PreferredBackBufferHeight / 2);
+        };
+
+        _ships = _shipsMeta.Select(s =>
         {
             var shipObj = new GameObject();
 
@@ -80,8 +122,12 @@ public class ShipSelectionScene : Scene
 
             shipObj.Enabled = false;
 
+            shipsContainer.AddChild(shipObj);
+
             return shipObj;
         }).ToArray();
+
+        this.Root.AddChild(shipsContainer);
     }
 
     private GameObject BuildBackground()
