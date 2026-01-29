b598befa147856284ad483aaa8ee8c23d789c5eb
David Guida <1432872+mizrael@users.noreply.github.com>
2026-01-28 21:21:51 -0500

    small fix

--- games/Solocaster/UI/CharacterPanel.cs
+++ games/Solocaster/UI/CharacterPanel.cs
@@ -8,9 +8,11 @@ namespace Solocaster.UI;
 public class CharacterPanel : Widget
 {
     private const int PanelSpacing = 10;
+    private const int BeltTopMargin = 15;
 
     private readonly StatsPanel _statsPanel;
     private readonly InventoryPanel _inventoryPanel;
+    private readonly BeltPanel _beltPanel;
 
     public CharacterPanel(
         InventoryComponent inventory,
@@ -21,17 +23,28 @@ public class CharacterPanel : Widget
     {
         _statsPanel = new StatsPanel(stats, font, game);
         _inventoryPanel = new InventoryPanel(inventory, dragDropManager, font, game);
+        _beltPanel = new BeltPanel(inventory, dragDropManager, font, game);
 
         _statsPanel.Position = Vector2.Zero;
         _inventoryPanel.Position = new Vector2(_statsPanel.Size.X + PanelSpacing, 0);
 
+        float topRowHeight = MathHelper.Max(_statsPanel.Size.Y, _inventoryPanel.Size.Y);
+
+        _beltPanel.Position = new Vector2(
+            _inventoryPanel.Position.X + (_inventoryPanel.Size.X - _beltPanel.Size.X) / 2,
+            topRowHeight + BeltTopMargin
+        );
+
+        float totalHeight = _beltPanel.Position.Y + _beltPanel.Size.Y;
+
         Size = new Vector2(
             _statsPanel.Size.X + PanelSpacing + _inventoryPanel.Size.X,
-            MathHelper.Max(_statsPanel.Size.Y, _inventoryPanel.Size.Y)
+            totalHeight
         );
 
         AddChild(_statsPanel);
         AddChild(_inventoryPanel);
+        AddChild(_beltPanel);
 
         Visible = false;
     }
